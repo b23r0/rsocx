af9574f6b0b3303b8e90aee68f243076d5d9334e
b23r0 <b23r0@foxmail.com>
2021-11-10 19:02:01 +0800

    optimized code structure.

--- src/utils.rs
+++ src/utils.rs
@@ -1,9 +1,9 @@
-pub static MAGIC_FLAG : [u8;2] = [0x37, 0x37];
+//pub static MAGIC_FLAG : [u8;2] = [0x37, 0x37];
 
 pub fn makeword(a : u8, b : u8) -> u16{
     return ((a as u16) << 8) | b as u16;
 }
-
+/*
 pub fn splitword(a : u16) -> (u8 , u8) {
     return ((a >> 8) as u8  , a as u8)
-}
+}*/
