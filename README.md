246b5d512c8cc1c7e32e085f8d63132c69633e10
b23r0 <b23r0@foxmail.com>
2021-11-13 13:22:26 +0800

    alpha version

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# rsocx
-Real reverse socks5 proxy implementation in Rust.
+# rsocx [![Build Status](https://app.travis-ci.com/b23r0/rsocx.svg?branch=main)](https://app.travis-ci.com/b23r0/rsocx) [![ChatOnDiscord](https://img.shields.io/badge/chat-on%20discord-blue)](https://discord.gg/ZKtYMvDFN4)
+A high performence Socks5 proxy with bind/reverse support implementation by Rust
+
+# Features
+
+* Async-std
+* No unsafe code
+* Support reverse mode(Not bind any port in client)
+
+# Build & Run
+
+`$> cargo build --release`
+
+`$> ./target/release/rsocx`
+
+# Usage
+
+## Bind Mode
+
+You can run a socks5 proxy and listen port at 1080
+
+`$> ./rsocx -l 1080`
+
+## Reverse Mode
+
+First listen a port waiting for slave connection
+
+`$> ./rsocx -l 8000 1080`
+
+then reverse connect to master in slave
+
+`$> ./cliws -r 127.0.0.1 8000`
+
+# Reference
+
+* https://github.com/ajmwagar/merino
+
+* https://github.com/kost/revsocks
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,18 @@
+include!("utils.rs");
 mod socks;
 
 use log::LevelFilter;
 use simple_logger::SimpleLogger;
 
-use async_std::{io, net::{TcpListener}, prelude::*, task};
+use futures::{AsyncReadExt, AsyncWriteExt, FutureExt, StreamExt};
+use async_std::{io, net::{TcpListener, TcpStream}, task};
+use futures::select;
+
 
 fn usage() {
-    return;
+	println!("rsocx - A high performence Socks5 proxy with bind/reverse support implementation by Rust");
+	println!("https://github.com/b23r0/rsocx");
+	println!("Usage: rsocx [-l socks5 port] [-t [slave_port] [socks5 port]] [-r [master ip] [master port]]");
 }
 
 
@@ -28,7 +34,7 @@ async fn main() -> io::Result<()>  {
 		"-l" => {
 			let port = match std::env::args().nth(2){
 				None => {
-					log::error!("not found listen port . eg : cliws -l 8000");
+					log::error!("not found listen port . eg : rsocx -l 8000");
 					return Ok(());
 				},
 				Some(p) => p
@@ -51,6 +57,177 @@ async fn main() -> io::Result<()>  {
 					socks::socksv5_handle(stream).await;
 				});
 			}
+		},
+		"-t" => {
+			let master_port = match std::env::args().nth(2){
+				None => {
+					log::error!("not found listen port . eg : rsocx -t 9000 8000");
+					return Ok(());
+				},
+				Some(p) => p
+			};
+			let socks_port = match std::env::args().nth(3){
+				None => {
+					log::error!("not found listen port . eg : rsocx -t 9000 8000");
+					return Ok(());
+				},
+				Some(p) => p
+			};
+
+			log::info!("listen to : {} waiting for slave" , "0.0.0.0:".to_string() + &master_port);
+			
+			let slave_listener = match TcpListener::bind("0.0.0.0:".to_string() + &master_port).await{
+				Err(e) => {
+					log::error!("error : {}", e);
+					return Ok(());
+				},
+				Ok(p) => p
+			};
+
+			let (mut slave_stream , slave_addr) = match slave_listener.accept().await{
+				Err(e) => {
+					log::error!("error : {}", e);
+					return Ok(());
+				},
+				Ok(p) => p
+			};
+
+			log::info!("accept slave connection from : {}:{}" , slave_addr.ip() , slave_addr.port() );
+
+			log::info!("listen to : {}" , "0.0.0.0:".to_string() + &socks_port);
+			
+			let listener = match TcpListener::bind("0.0.0.0:".to_string() + &socks_port).await{
+				Err(e) => {
+					log::error!("error : {}", e);
+					return Ok(());
+				},
+				Ok(p) => p
+			};
+
+			let mut incoming = listener.incoming();
+
+			while let Some(stream) = incoming.next().await {
+				let mut stream = stream?;
+
+				match slave_stream.write_all(&[MAGIC_FLAG[0] , MAGIC_FLAG[1]]).await{
+					Err(e) => {
+						log::error!("error : {}" , e);
+						break;
+					}
+					_ => {}
+				};
+
+				let (mut proxy_stream , slave_addr) = match slave_listener.accept().await{
+					Err(e) => {
+						log::error!("error : {}", e);
+						return Ok(());
+					},
+					Ok(p) => p
+				};
+
+				log::info!("accept from slave proxy connection : {}:{}" , slave_addr.ip() , slave_addr.port() );
+
+				task::spawn(async move {
+					let mut buf1 = [0u8 ; 1024];
+					let mut buf2 = [0u8 ; 1024];
+
+					loop{
+						select! {
+							a = proxy_stream.read(&mut buf1).fuse() => {
+			
+								let len = match a {
+									Err(e) => {
+										log::error!("error : {}" , e);
+										break;
+									}
+									Ok(p) => p
+								};
+								match stream.write_all(&mut buf1[..len]).await {
+									Err(e) => {
+										log::error!("error : {}" , e);
+										break;
+									}
+									Ok(p) => p
+								};
+			
+								if len == 0 {
+									break;
+								}
+							},
+							b = stream.read(&mut buf2).fuse() =>  { 
+								let len = match b{
+									Err(e) => {
+										log::error!("error : {}" , e);
+										break;
+									}
+									Ok(p) => p
+								};
+								match proxy_stream.write_all(&mut buf2[..len]).await {
+									Err(e) => {
+										log::error!("error : {}" , e);
+										break;
+									}
+									Ok(p) => p
+								};
+								if len == 0 {
+									break;
+								}
+							},
+							complete => break,
+						}
+					}
+				});
+			}
+		},
+		"-r" => {
+			let addr = match std::env::args().nth(2){
+				None => {
+					log::error!("not found connection ip . eg : rsocx -r 192.168.0.1 9000");
+					return Ok(());
+				},
+				Some(p) => p
+			};
+			let port = match std::env::args().nth(3){
+				None => {
+					log::error!("not found connection port . eg : rsocx -r 192.168.0.1 9000");
+					return Ok(());
+				},
+				Some(p) => p
+			};
+			let fulladdr = format!("{}:{}" , addr , port);
+			let mut master_stream = match TcpStream::connect(fulladdr.clone()).await{
+				Err(e) => {
+					log::error!("error : {}", e);
+					return Ok(());
+				},
+				Ok(p) => p
+			};
+
+			loop {
+				let mut buf = [0u8 ; 2];
+				match master_stream.read_exact(&mut buf).await{
+					Err(e) => {
+						log::error!("error : {}", e);
+						return Ok(());
+					},
+					Ok(p) => p
+				};
+	
+				if buf[0] == MAGIC_FLAG[0] && buf[1] == MAGIC_FLAG[1] {
+					let stream = match TcpStream::connect(fulladdr.clone()).await{
+						Err(e) => {
+							log::error!("error : {}", e);
+							return Ok(());
+						},
+						Ok(p) => p
+					};
+
+					task::spawn(async {
+						socks::socksv5_handle(stream).await;
+					});
+				}
+			}
+
 		},
 		_ => {
 			usage();
